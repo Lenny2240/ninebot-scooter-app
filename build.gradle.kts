52d4a48a5b44311e8ca9e5bfffe5ae06d2b6936b
Lenny2240 <maui-katze@gmx.de>
2026-07-19 12:54:10 +0200

    Initial: Root build configuration with Kotlin and Jetpack Compose setup

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+// Root build.gradle.kts
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+    kotlin("android") version "1.9.20" apply false
+    kotlin("plugin.serialization") version "1.9.20" apply false
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
